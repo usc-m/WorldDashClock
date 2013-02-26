db0355accf39b6dbf5b5761e023f882aea0f66eb
Matthew Summers <matt@wishf.co.uk>
2013-02-26 20:49:28 +0000

    Initial code commit

---
+++ src/uk/co/wishf/worlddashclock/WorldClockExtension.java
@@ -0,0 +1,99 @@
+package uk.co.wishf.worlddashclock;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+public class WorldClockExtension extends DashClockExtension {
+	
+	static final String TZ_PREF = "timezone";
+	
+	Timer timer;
+	
+	String userTz;
+	
+	protected synchronized String getTz() {
+		return userTz;
+	}
+	
+	private synchronized void setTz(String tz) {
+		userTz = tz;
+	}
+	
+	@Override
+	protected void onInitialize(boolean isReconnect) {
+		this.setUpdateWhenScreenOn(true);
+		
+		if(timer == null) {
+			timer = new Timer("World Clock Dashclock Extension Updater");
+			Calendar now = Calendar.getInstance();
+			long dly = (1000 - now.get(Calendar.MILLISECOND)) + (1000 * (60 - now.get(Calendar.SECOND)));
+			timer.scheduleAtFixedRate(new ClockUpdater(this), dly, 60*1000);
+		}
+		
+		super.onInitialize(isReconnect);
+	}
+	
+	@Override
+	protected void onUpdateData(int reason) {
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+		this.setTz(sp.getString(TZ_PREF, "Australia/Sydney"));
+		
+		this.publishUpdate(generateExtensionData(this.getTz()));
+	}
+	
+	private static ExtensionData generateExtensionData(String tzName) {
+		final TimeZone tz = TimeZone.getTimeZone(tzName);
+		final Calendar tzTime = Calendar.getInstance(tz);
+		
+		String[] nameComponents = tzName.replace('_', ' ').split("/");
+		String placeName = String.format("%s, %s", nameComponents[1], nameComponents[0]);
+		
+		StringBuilder status = new StringBuilder();
+		status.append(tzTime.get(Calendar.HOUR_OF_DAY));
+		status.append(':');
+		status.append(tzTime.get(Calendar.MINUTE));
+		
+		StringBuilder title = new StringBuilder();
+		title.append(tzTime.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault()));
+		title.append(", ");
+		title.append(tzTime.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()));
+		title.append(' ');
+		title.append(tzTime.get(Calendar.DAY_OF_MONTH));
+		title.append(" at ");
+		title.append(status.toString());
+
+		
+		return new ExtensionData()
+				   .visible(true)
+				   .icon(R.drawable.clock)
+				   .status(status.toString())
+				   .expandedTitle(title.toString())
+				   .expandedBody(placeName);
+	}
+	
+	class ClockUpdater extends TimerTask {
+		
+		WorldClockExtension parent;
+		
+		public ClockUpdater(WorldClockExtension instance) {
+			parent = instance;
+		}
+		
+		@Override
+		public void run() {
+			parent.publishUpdate(generateExtensionData(parent.getTz()));
+		}
+		
+	}
+
+}
---
+++ src/uk/co/wishf/worlddashclock/WorldClockSettingsActivity.java
@@ -0,0 +1,130 @@
+package uk.co.wishf.worlddashclock;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.RingtonePreference;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import java.util.List;
+import java.util.TimeZone;
+
+public class WorldClockSettingsActivity extends PreferenceActivity {
+	/**
+	 * Determines whether to always show the simplified settings UI, where
+	 * settings are presented in a single list. When false, settings are shown
+	 * as a master/detail two-pane view on tablets. When true, a single pane is
+	 * shown on tablets.
+	 */
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActionBar().setIcon(R.drawable.clock);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+
+		setupSimplePreferencesScreen();
+	}
+
+	/**
+	 * Shows the simplified settings UI if the device configuration if the
+	 * device configuration dictates that a simplified, single-pane UI should be
+	 * shown.
+	 */
+	private void setupSimplePreferencesScreen() {
+		// In the simplified UI, fragments are not used at all and we instead
+		// use the older PreferenceActivity APIs.
+
+		// Add 'general' preferences.
+		addPreferencesFromResource(R.xml.pref_general);
+		
+		String[] timezones = TimeZone.getAvailableIDs();
+		
+		ListPreference listPref = new ListPreference(this);
+		listPref.setKey(WorldClockExtension.TZ_PREF);
+		listPref.setEntries(timezones);
+		listPref.setEntryValues(timezones);
+		listPref.setDialogTitle("Select Timezone");
+		listPref.setTitle("Timezone");
+		
+		getPreferenceScreen().addPreference(listPref);
+		
+		this.bindPreferenceSummaryToValue(listPref);
+	}
+
+	/**
+	 * A preference value change listener that updates the preference's summary
+	 * to reflect its new value.
+	 */
+	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+			String stringValue = value.toString();
+
+			if (preference instanceof ListPreference) {
+				// For list preferences, look up the correct display value in
+				// the preference's 'entries' list.
+				ListPreference listPreference = (ListPreference) preference;
+				int index = listPreference.findIndexOfValue(stringValue);
+
+				// Set the summary to reflect the new value.
+				preference
+						.setSummary(index >= 0 ? listPreference.getEntries()[index]
+								: null);
+			}
+			
+			return true;
+		}
+	};
+
+	/**
+	 * Binds a preference's summary to its value. More specifically, when the
+	 * preference's value is changed, its summary (line of text below the
+	 * preference title) is updated to reflect the value. The summary is also
+	 * immediately updated upon calling this method. The exact display format is
+	 * dependent on the type of preference.
+	 * 
+	 * @see #sBindPreferenceSummaryToValueListener
+	 */
+	private static void bindPreferenceSummaryToValue(Preference preference) {
+		// Set the listener to watch for value changes.
+		preference
+				.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+		// Trigger the listener immediately with the preference's
+		// current value.
+		sBindPreferenceSummaryToValueListener.onPreferenceChange(
+				preference,
+				PreferenceManager.getDefaultSharedPreferences(
+						preference.getContext()).getString(preference.getKey(),
+						""));
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        case android.R.id.home:
+	        	//TODO: Preserve stack
+	            finish();
+	            return true;
+	    }
+	    return super.onOptionsItemSelected(item);
+	}
+}
+
